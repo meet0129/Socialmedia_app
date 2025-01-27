ee2f8a228c01b5e635cc8ebf201161877461e8f4
meet0129 <meetpbarasara129@gmail.com>
2025-01-28 02:44:05 +0530

    Socialmedia

---
+++ SnapSpot/src/_auth/Authlayout.tsx
@@ -0,0 +1,21 @@
+import {Outlet,Navigate} from 'react-router-dom';
+
+const Authlayout = () => {
+  const isAuthenticated =false;
+  
+  return (
+    <>
+    {isAuthenticated ? (
+      <Navigate to='/' />
+    )
+    : (
+      <>
+      <section>
+        <Outlet />
+      </section>
+      </>
+    )}
+    </>
+  )
+}
+export default Authlayout
