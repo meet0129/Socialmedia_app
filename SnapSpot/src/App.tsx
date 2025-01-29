7b3359b41ba09926f9ac73753cf5e4121047d650
meet0129 <meetpbarasara129@gmail.com>
2025-01-29 23:48:49 +0530

    before tanstack

--- SnapSpot/src/App.tsx
+++ SnapSpot/src/App.tsx
@@ -1,10 +1,11 @@
 import { Routes ,Route } from 'react-router-dom';
-import './globals.css';
-import SigninForm from './_auth/forms/SigninForm';
 import { Home } from './_root/pages';
+import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
 import Authlayout from './_auth/Authlayout';
 import RootLayout from './_root/RootLayout';
+import './globals.css';
+import { Toaster } from "@/components/ui/toaster"
 
 const App = () => {
   return (
@@ -20,9 +21,8 @@ const App = () => {
           <Route element={<RootLayout />}>
                 <Route index element={<Home/>} />
           </Route>
-
         </Routes>
-        
+        <Toaster />
     </main>
   )
 }
---
+++ SnapSpot/src/lib/appwrite/api.ts
@@ -0,0 +1,55 @@
+import { INewUser } from "@/types";
+import { ID } from 'appwrite';
+import { account, appwriteConfig, avatars, databases } from "./config";
+
+export async function createUserAccount(user: INewUser) {
+    try {
+        const newAccount = await account.create(
+            ID.unique(),
+            user.email,
+            user.password,
+            user.name
+        );
+        if (!newAccount) throw Error;
+
+        // Get the initials as a string
+        const avatarUrlString = avatars.getInitials(user.name);
+
+        // Convert the string to a URL object
+        const avatarUrl = new URL(avatarUrlString);
+
+        const newUser = await saveUserToDB({
+            accountId: newAccount.$id,
+            name: newAccount.name,
+            email: newAccount.email,
+            username: user.username,
+            imageUrl: avatarUrl, // Now it's a URL object
+        });
+
+        return newUser;
+
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
+export async function saveUserToDB(user: {
+    accountId: string;
+    email: string;
+    name: string;
+    imageUrl: URL; // Accepts URL type
+    username?: string;
+}) {
+    try {
+        const newUser = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            ID.unique(),
+            user,
+        );
+        return newUser;
+    } catch (error) {
+        console.log(error);
+    }
+}
